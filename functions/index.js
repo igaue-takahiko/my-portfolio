7b936b8e085a69a9d3a9ae5062e27362c67e95ce
igaue-takahhiko <hasewo.2610@gmail.com>
2021-01-09 19:38:40 +0900

    remake layout

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+
+const sendResponse = (res, statusCode, body) => {
+    res.send({
+        statusCode,
+        body: JSON.stringify(body)
+    })
+};
+
+exports.addDataset = functions.https.onRequest(async (req, res) => {
+    if (req.method !== "POST") {
+        sendResponse(res, 405, { error: "Invalid Request" })
+    } else {
+        const dataset = req.body;
+        for (const key of Object.keys(dataset)) {
+            const data = dataset[key];
+            db.collection("questions").doc(key).set(data)
+        }
+        sendResponse(res, 200, { message: "Successfully added dataset!" })
+    }
+});
